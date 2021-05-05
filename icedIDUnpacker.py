f2acb1190453e4b5a422c0fa4b1a1ed0818d34dd
4rchib4ld <65158158+4rchib4ld@users.noreply.github.com>
2021-05-05 10:23:59 +0200

    Update icedIDUnpacker.py
    Adding code emulation as well as more automatic checks

--- icedIDUnpacker.py
+++ icedIDUnpacker.py
@@ -1,3 +1,9 @@
+from __future__ import print_function
+from unicorn import *
+from unicorn.x86_const import *
+from pathlib import PurePath
+import pickle
+import yara
 import binascii
 import pefile
 import argparse
@@ -5,15 +11,14 @@ import argparse
 def extractRdataSect(pe):
     # Extracting the payload from the .rdata section
     print("[+] Extracting rdata...")
+    startOfDebugDirectory = pe.OPTIONAL_HEADER.DATA_DIRECTORY[pefile.DIRECTORY_ENTRY["IMAGE_DIRECTORY_ENTRY_DEBUG"]].VirtualAddress
     for section in pe.sections:
         if ".rdata" in str(section.Name):
             rdata = section.get_data()
             rdata = rdata[0x80:]
-            for i in range(len(rdata)):
-                # Searching for the end of the chunk. Better than a hardcoded value
-                if rdata[i] == 0 and rdata[i+1] == 0 and rdata[i+2] == 0 and rdata[i+3] == 0:
-                    print("[+] Done !")
-                    return rdata[:i]
+            RdataVirtualAddress = section.VirtualAddress
+            endOfPayload = startOfDebugDirectory - RdataVirtualAddress - 0x80
+            return rdata[:endOfPayload]
             
 def extractDataSect(pe):
     # Extracting the payload from the .data section
@@ -34,7 +39,7 @@ def rdataDecode(rdata):
 def rdataDecrypt(decodedRdata):
     # OK this one is pretty annoying. Starting from the end for the data, and a byte every 20 bytes. Then it loops again, but from len(data)-1 and so on
     payload = bytearray()
-    decrem = 0x14 # Maybe this will change ?
+    decrem = decodedRdata[-1] # That's where the value is located
     count = 0
     scount = 0
     lenRdata = len(decodedRdata) - 1
@@ -50,19 +55,60 @@ def rdataDecrypt(decodedRdata):
             i = lenRdata - scount
     return payload[::-1]
 
-def firstDecrypt(decodedRdata):
-    # Pretty straight forward. There is a key, there is a byte, a bunch of bitwise operations
-    firstDecr = bytearray()
-    count=0
-    key = 1 # Maybe this will change ?
-    for byte in decodedRdata:
-        if count == 512:
-            key += 1
-            count = 0
-        result = ((~byte & 0xC) | (byte & 0xf3)) ^ ((~key & 0xC) | (key & 0xF3))
-        count += 1
-        firstDecr.append(result)
-    return firstDecr[::1]
+def gettingObfuscationCode(file):
+    rules = yara.compile(source='''rule IceLoaderPacker {
+    strings:
+        $IceLoaderPacker = {89 DA [0-7] B? FF 44 30 [0-17] C2 44 30 [0-8] 20 ?? 08 D0 [0-8] 88 84}
+    condition:
+        all of them
+    }''')
+    with open(file, "rb") as f:
+        sample = f.read()
+        matches = rules.match(data=sample)
+        start = int(matches[0].strings[0][0])
+        end = start + len(matches[0].strings[0][2])
+        return sample[start:end]
+
+def runObfuscationCode(decodedRdata, encrCode):
+    X86_CODE64 = encrCode
+    ADDRESS = 0x1000000
+    try:
+        # Initialize emulator in X86-64bit mode
+        mu = Uc(UC_ARCH_X86, UC_MODE_64)
+
+        # map 2MB memory for this emulation
+        mu.mem_map(ADDRESS, 2 * 1024 * 1024)
+
+        # write machine code to be emulated to memory
+        mu.mem_write(ADDRESS, X86_CODE64)
+
+        # setup stack
+        mu.reg_write(UC_X86_REG_RSP, ADDRESS + 0x200000)
+        firstDecr = bytearray()
+        count = 0
+        key = 1 # Maybe this will change ?
+        for byte in decodedRdata:
+            if count == 512:
+                key += 1
+                count = 0
+            # initialize machine registers
+            mu.reg_write(UC_X86_REG_RAX, byte)
+            mu.reg_write(UC_X86_REG_RBX, key)
+            mu.reg_write(UC_X86_REG_RDX, 0x0)
+
+            try:
+                # emulate machine code in infinite time
+                mu.emu_start(ADDRESS, ADDRESS + len(X86_CODE64))
+            except UcError as e:
+                print("ERROR: %s" % e)
+
+            result = mu.reg_read(UC_X86_REG_RAX)
+            count += 1
+            firstDecr.append(result)
+        return firstDecr[::1]
+
+    except UcError as e:
+        print("ERROR: %s" % e)
 
 def decryptSecondStage(encryptedPayload, dataSect):
     # The final decryption. Loop throught the data section and take two bytes at a time, adding them and getting the corresponding char in the decrypted payload from .rdata
@@ -94,7 +140,7 @@ def extractPayload(file):
 def decodePayload(payload):
     decrypted = ""
     for i in range(32):
-     decrypted += chr(payload[i+64] ^ payload[i])
+        decrypted += chr(payload[i+64] ^ payload[i])
     return decrypted.split("\x00")[0]
 
 def main():
@@ -105,14 +151,17 @@ def main():
     rdata = extractRdataSect(pe)
     data = extractDataSect(pe)
     decryptedRdata = rdataDecrypt(rdataDecode(rdata))
-    encryptedPayload = firstDecrypt(decryptedRdata)
+    obfCode = gettingObfuscationCode(args.file)
+    obfCode = obfCode[:-2]
+    encryptedPayload = runObfuscationCode(decryptedRdata, obfCode)
     secondStage = decryptSecondStage(encryptedPayload, data)
     payload = extractPayload(secondStage)
     config = decodePayload(payload[4:]) #skipping the first 4 bytes
     print(f"The C2 config is : {config}")
-    with open("unpackedIcedID.bin", "wb") as f:
+    filename = PurePath(args.file).parts
+    filename = filename[-1]
+    with open(f"unpacked_{filename}", "wb") as f:
         f.write(secondStage)
 
 if __name__ == "__main__":
     main()
-
